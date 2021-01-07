d73b8dc1771f0f2712c9930728e0eb267e43f0aa
Omar Cossio <omar119mce@gmail.com>
2021-01-06 21:06:16 -0500

    m

--- server.js
+++ server.js
@@ -17,15 +17,11 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 });
 
 // routes
-//use the next line when using local system server
 app.use(require("./routes/api.js"));
-//app.use('/api', require("./routes/api.js"));
-//use this next time when using heroku
-//app.use(require("routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);

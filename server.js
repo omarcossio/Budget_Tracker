2edd1be8d1560695b5cef4ae9e7378c5e88ba42f
Omar Cossio <omar119mce@gmail.com>
2021-01-06 20:26:02 -0500

    added possible solution for heroku api problem to server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 const app = express();
 
 app.use(logger("dev"));
